7c6c1349b9d5156203176d77a6ebacfbdb6394f3
loic-job-dev <loicjobard@hotmail.com>
2025-08-12 11:21:49 +0200

    First step done

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        int boxesToMove = 34;
+        while (boxesToMove>0) {
+            if (boxesToMove>=9) {
+                boxesToMove =  boxesToMove-9;
+                System.out.println("un voyage de 9 cartons");
+            }
+            else {
+                System.out.println("un voyage de " + boxesToMove + " cartons");
+                boxesToMove = 0;
+            }
+        }
     }
 }
