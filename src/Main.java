442f87bd7aefc5411e9091866c078bdefb40cd55
loic-job-dev <loicjobard@hotmail.com>
2025-08-12 11:57:41 +0200

    Moving out with dynamic values

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,57 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int boxesToMove = 34;
+        int boxesToMove = 0;
+        int truckCapacity = 0;
+        boolean isInt = false;
+
+        Scanner clavier = new Scanner(System.in);
+
+        //Définition et vérification du nombre de cartons
+        while (!isInt) {
+            System.out.println("Saisir le nombre de catons : ");
+            //Si la saisie est un int
+            if (clavier.hasNextInt()) {
+                boxesToMove = clavier.nextInt();
+                isInt = true;
+            }
+            //Sinon on boucle
+            else {
+                System.out.println("Merci de saisir un nombre entier");
+                clavier.next();
+            }
+        }
+
+        //Réinitialisation du booléen isInt
+        isInt = false;
+
+        //Définition et vérification de la capacité du camion
+        while (!isInt) {
+            System.out.println("Saisir la capacité du camion : ");
+            //Si la saisie est un int
+            if (clavier.hasNextInt()) {
+                truckCapacity = clavier.nextInt();
+                isInt = true;
+            }
+            //Sinon on boucle
+            else {
+                System.out.println("Merci de saisir un nombre entier");
+                clavier.next();
+            }
+        }
+
+        //Déménagement
         while (boxesToMove>0) {
-            if (boxesToMove>=9) {
-                boxesToMove =  boxesToMove-9;
-                System.out.println("un voyage de 9 cartons");
+            if (boxesToMove>=truckCapacity) {
+                boxesToMove =  boxesToMove-truckCapacity;
+                System.out.println("un voyage de " + truckCapacity + " cartons");
             }
             else {
                 System.out.println("un voyage de " + boxesToMove + " cartons");
                 boxesToMove = 0;
             }
         }
+        clavier.close();
     }
 }
