918598fc046bd70a217dd2bb1b5f8329645142c9
loic-job-dev <loicjobard@hotmail.com>
2025-08-12 14:33:28 +0200

    filled README.md and refactorised code

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,64 @@
 import java.util.Scanner;
-
+/**
+ * Project created in order to learn Java basics
+ *
+ *
+ *  @author Loic
+ *  @version 1.0
+ *  @since 2025-08-12
+ */
 public class Main {
 
     static int boxesToMove = 0;
     static int truckCapacity = 0;
-    //Définition de la fonction utilitaire
+
+    /**
+     * Asks for the user to give an integer, and returns it if the value is correct.
+     *
+     * @return the value of the integer given by the user.
+     */
     private static int getInt() {
         Scanner clavier = new Scanner(System.in);
 
         while (!clavier.hasNextInt()) {
-            System.out.println("Merci de saisir un nombre entier");
+            afficherMessage("Merci de saisir un nombre entier");
             clavier.next();
         }
         return clavier.nextInt();
     }
 
-    private static void afficherMessage() {
-        if (boxesToMove == 0) {
-            System.out.println("Merci de renseigner le nombre de cartons :");
-        } else if (boxesToMove != 0 && truckCapacity == 0) {
-            System.out.println("Merci de renseigner la capacité maximale du camion :");
-        }
-        else if (boxesToMove != 0 && truckCapacity != 0 && boxesToMove >= truckCapacity) {
-            System.out.println("un voyage de " + truckCapacity + " cartons");
-        }
-        else if (boxesToMove != 0 && truckCapacity != 0 && boxesToMove < truckCapacity) {
-            System.out.println("un voyage de " + boxesToMove + " cartons");
-        }
+    /**
+     * Displays a message in the console.
+     *
+     * @param message is the String that will be displayed
+     */
+    private static void afficherMessage(String message) {
+        System.out.println(message);
     }
 
+    /**
+     * Entry point of the program.
+     * Asks the user to give a number of boxes and the maximum capacity of the truck
+     * Then it displays each travel of the truck with the number of boxes moved, until it reaches 0.
+     *
+     * @param args arguments given in terminal
+     */
     public static void main(String[] args) {
 
         //Définition et vérification du nombre de cartons
-        afficherMessage();
+        afficherMessage("Veuillez renseigner le nombre de cartons");
         boxesToMove = getInt();
         //Définition et vérification de la capacité du camion
-        afficherMessage();
+        afficherMessage("Veuillez renseigner la capacité maximale du camion");
         truckCapacity = getInt();
 
-        //Déménagement
         while (boxesToMove >0) {
             if (boxesToMove >= truckCapacity) {
-                afficherMessage();
+                afficherMessage("Un voyage de " + truckCapacity + " cartons");
                 boxesToMove =  boxesToMove - truckCapacity;
             }
             else {
-                afficherMessage();
+                afficherMessage("Un voyage de " + boxesToMove + " cartons");
                 boxesToMove = 0;
             }
         }
