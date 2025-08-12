6cc9fb72c8ed76603ef0e684e24c2f9f7ecc879e
loic-job-dev <loicjobard@hotmail.com>
2025-08-12 14:06:18 +0200

    try to have less Sysout

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Demenagement_java
+
+## Project created in order to learn Java basics
+
+This program asks the user to  give a number of boxes (it must be an integer) ans the maximum capacity of the truck (must be an integer too).
+
+When done, the program lists the travels done by the truck with the number of boxes that are moved, until the moving out is finished.
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int boxesToMove = 0;
+    static int truckCapacity = 0;
     //Définition de la fonction utilitaire
     private static int getInt() {
         Scanner clavier = new Scanner(System.in);
@@ -13,24 +15,37 @@ public class Main {
         return clavier.nextInt();
     }
 
+    private static void afficherMessage() {
+        if (boxesToMove == 0) {
+            System.out.println("Merci de renseigner le nombre de cartons :");
+        } else if (boxesToMove != 0 && truckCapacity == 0) {
+            System.out.println("Merci de renseigner la capacité maximale du camion :");
+        }
+        else if (boxesToMove != 0 && truckCapacity != 0 && boxesToMove >= truckCapacity) {
+            System.out.println("un voyage de " + truckCapacity + " cartons");
+        }
+        else if (boxesToMove != 0 && truckCapacity != 0 && boxesToMove < truckCapacity) {
+            System.out.println("un voyage de " + boxesToMove + " cartons");
+        }
+    }
+
     public static void main(String[] args) {
 
         //Définition et vérification du nombre de cartons
-        System.out.println("Merci de renseigner le nombre de cartons :");
-        int boxesToMove = getInt();
+        afficherMessage();
+        boxesToMove = getInt();
         //Définition et vérification de la capacité du camion
-        System.out.println("Merci de renseigner la capacité maximale du camion :");
-        int truckCapacity = getInt();
-
+        afficherMessage();
+        truckCapacity = getInt();
 
         //Déménagement
         while (boxesToMove >0) {
             if (boxesToMove >= truckCapacity) {
+                afficherMessage();
                 boxesToMove =  boxesToMove - truckCapacity;
-                System.out.println("un voyage de " + truckCapacity + " cartons");
             }
             else {
-                System.out.println("un voyage de " + boxesToMove + " cartons");
+                afficherMessage();
                 boxesToMove = 0;
             }
         }
